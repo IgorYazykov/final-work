385c0c4dc21baf81d2eec82073da0d6d32d4e7d1
Igor <gwscdf110@gmail.com>
2022-10-23 09:40:44 +0300

    some fix for menu

--- script/script_pop_up.js
+++ script/script_pop_up.js
@@ -1,8 +1,17 @@
 const modal_button = document.getElementById("modal_button");
 const close = document.getElementById("close");
 const modal = document.getElementById("modal");
+const modal_button_1 = document.getElementById("modal_button_1");
 
 modal_button.addEventListener("click", function(){
+    modal.classList.add("active");
+})
+
+close.addEventListener("click", function(){
+    modal.classList.remove("active");
+})
+
+modal_button_1.addEventListener("click", function(){
     modal.classList.add("active");
 })
 
